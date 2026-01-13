ae8dc7ddbc83d4efea77dc6d3b79afb8c28ce4da
Harsh Gautam <harshgautam@gmail.com>
2026-01-14 00:05:43 +0530

    commit try

--- src/TwoDimArray/ForEach2D.java
+++ src/TwoDimArray/ForEach2D.java
@@ -3,15 +3,22 @@ package TwoDimArray;
 public class ForEach2D {
     public static void main(String[] args) {
         int[][] arr = {  {1,2,3},
-                {4,5,6},
-                {7,8,9},
-                {4,2,7}};
+                         {4,5,6},
+                         {7,8,9},
+                         {4,2,7} };
 
-        for(int i = 0; i < arr.length;i++){
-            for(int ele : arr[i]){
+        for(int[] a : arr){
+            for(int ele : a){
                 System.out.print(ele + " ");
             }
             System.out.println();
         }
+
+//        for(int i = 0; i < arr.length;i++){
+//            for(int ele : arr[i]){
+//                System.out.print(ele + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
