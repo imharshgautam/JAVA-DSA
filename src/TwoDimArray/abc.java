6c302e55c9fb294b8b7fa2bb6141b3e2ede59e4a
Harsh Gautam <meharshgautam@gmail.com>
2026-01-14 13:01:53 +0530

    commit try

--- src/TwoDimArray/abc.java
+++
@@ -1,7 +0,0 @@
-package TwoDimArray;
-
-public class abc {
-    public static void main(String[] args) {
-        System.out.println("Harsh");
-    }
-}
--- src/TwoDimArray/harsh.java
+++
@@ -1,5 +0,0 @@
-package TwoDimArray;
-
-public class harsh {
-    sbjhbasj
-}
