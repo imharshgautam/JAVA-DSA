d3c564bc34e530db5d63b3c116ad763933dd6ddc
imharshgautam <meharshgautam@gmail.com>
2026-01-18 15:02:44 +0530

    commit try

---
+++ src/TwoDimArray/Search2dMatrix.java
@@ -0,0 +1,38 @@
+package TwoDimArray;
+
+public class Search2dMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 4, 7, 11},
+                {2, 5, 8, 12},
+                {3, 6, 9, 16},
+                {10, 13, 14, 17}
+        };
+
+        int target = 9;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target)
+                return true;
+            else if (matrix[i][j] > target)
+                j--;
+            else
+                i++;
+        }
+
+        return false;
+    }
+}
