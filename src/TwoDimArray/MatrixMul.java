ef22aa3a6ec44428521daa52e77ba32a684ac0b2
imharshgautam <meharshgautam@gmail.com>
2026-01-19 19:30:08 +0530

    commit try

--- src/TwoDimArray/MatrixMul.java
+++ src/TwoDimArray/MatrixMul.java
@@ -1,4 +1,47 @@
 package TwoDimArray;
 
 public class MatrixMul {
+        public static void main(String[] args) {
+
+            int[][] A = {
+                    {1, 2, 3},
+                    {4, 5, 6}
+            };
+
+            int[][] B = {
+                    {7, 8},
+                    {9, 10},
+                    {11, 12}
+            };
+
+            int r1 = A.length;
+            int c1 = A[0].length;
+            int r2 = B.length;
+            int c2 = B[0].length;
+
+            // Check condition
+            if (c1 != r2) {
+                System.out.println("Matrix multiplication not possible");
+                return;
+            }
+
+            int[][] C = new int[r1][c2];
+
+            // Multiply
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    for (int k = 0; k < c1; k++) {
+                        C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+
+            // Print result
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    System.out.print(C[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
 }
