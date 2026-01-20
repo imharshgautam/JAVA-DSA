ec3c1c0cd3161655f73d73b9be45f43b31d258e4
imharshgautam <meharshgautam@gmail.com>
2026-01-20 22:19:17 +0530

    commit try

--- src/Array/Questions/EvenOddOperation.java
+++
@@ -1,9 +0,0 @@
-package Array.Questions;
-import java.util.Scanner;
-
-public class EvenOddOperation {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
