ff8460c828215c804486497315e04c63214d48bc
imharshgautam <meharshgautam@gmail.com>
2026-01-21 01:35:32 +0530

    commit try

--- src/Strings/PalindromeString.java
+++ src/Strings/PalindromeString.java
@@ -1,4 +1,30 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int i = 0;
+        int j = s.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
