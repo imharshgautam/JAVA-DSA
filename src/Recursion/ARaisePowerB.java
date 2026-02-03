adcf3f6af146e44d8adc6fdad7bcdd0576a6258d
imharshgautam <meharshgautam@gmail.com>
2026-02-03 21:57:04 +0530

    commit try

---
+++ src/Recursion/ARaisePowerB.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class ARaisePowerB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = pow(a,b);
+        System.out.println(result);
+    }
+
+    public static int pow(int a, int b) {
+        if(b == 0) return 1;
+        return a*pow(a,b-1);
+
+    }
+}
