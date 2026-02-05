8d1eb85575479c29d320f2e205e092b84996ff4d
imharshgautam <meharshgautam@gmail.com>
2026-02-05 22:09:49 +0530

    commit try

--- src/Recursion/FibonacciNumber.java
+++ src/Recursion/FibonacciNumber.java
@@ -10,5 +10,7 @@ public class FibonacciNumber {
     public static void main(String[] args) {
         int n = 10;
         System.out.println(fib(n));
+        System.out.println(fib(n));
+
     }
 }
