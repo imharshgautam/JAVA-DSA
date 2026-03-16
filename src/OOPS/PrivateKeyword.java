58ba15dbdf633f55aec223b2ad014daed4e4bebe
imharshgautam <meharshgautam@gmail.com>
2026-03-16 20:46:28 +0530

    commit try

--- src/OOPS/PrivateKeyword.java
+++ src/OOPS/PrivateKeyword.java
@@ -1,4 +1,40 @@
 package OOPS;
 
+class Students{
+    String name;
+    private int rno = 23;
+    double cgpa;
+
+    private void print(){
+        System.out.println(name + " " + rno + " " + cgpa);
+    }
+
+    void p(){
+        print();
+    }
+
+    int getrno(){
+        return rno;
+    }
+
+    void setRno(int x){
+        rno = x;
+    }
+}
+
 public class PrivateKeyword {
+    public static void main(String[] args) {
+        Students s = new Students();
+
+        s.p();
+
+        s.name = "Harsh";
+        s.cgpa = 7.2;
+
+        s.p();
+
+        System.out.println(s.getrno());
+        s.setRno(5);
+        System.out.println(s.getrno());
+    }
 }
--- src/OOPS/UserDefineDataType.java
+++ src/OOPS/UserDefineDataType.java
@@ -1,4 +1,33 @@
 package OOPS;
 
+class Student{
+    String  name;
+    int rollno;
+    double cgpa;
+
+    void print(){
+        System.out.println(name + " " + rollno + " " + cgpa);
+    }
+}
+
 public class UserDefineDataType {
+
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Harsh";
+        s1.rollno = 23;
+        s1.cgpa = 7.2;
+
+//        System.out.println(s1.name+ " " + s1.rollno + " " +s1.cgpa);
+        s1.print();
+
+        Student s2 = new Student();
+        s2.name = "MJ";
+        s2.rollno = 34;
+        s2.cgpa = 8.2;
+
+//        System.out.println(s2.name+ " " + s2.rollno + " " +s2.cgpa);
+        s2.print();
+    }
 }
